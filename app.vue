653d87c08afd1e661c9989b156b3f8f9e8d6348b
NbekWeb <nbekdev@gmail.com>
2025-01-06 07:55:32 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <MainHead />
+    <NuxtPage />
+  </div>
+</template>
+<style scoped></style>
---
+++ components/main/head.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="flex justify-between">
+      <div>
+        <span class="logo">Logo</span>
+        <span>select</span>
+        <select>
+          <option>ru</option>
+          <option>en</option>
+        </select>
+      </div>
+      <div>
+        <span></span>
+        <span></span>
+        <button></button>
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
